361baba569e055454de3e253700769ec37d964f6
Decky Fajar Sidiq <46802206+deckyfajars@users.noreply.github.com>
2021-07-01 17:02:26 +0700

    Add files via upload

---
+++ tugas22.js
@@ -0,0 +1,9 @@
+function foreach(){
+  var data = ["Saya", "ingin", "belajar", "bersama"];
+  data.forEach(function (item, index){
+  console.log(item);
+  console.log(index);
+  })
+}
+
+foreach();
